c5814cafe0782b8c9924cb8c69b37b409aa797e6
raghveerdeol <deolraghveer5@gmail.com>
2024-06-25 15:54:53 +0200

    aggiunto paragrafo alla variabile e stampata la variabile in pagina

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    $var1 = "Tutti noi prima o poi moriamo. Questo significa forse che la vita è priva di significato? Significa che il fatto di essere venuti al mondo non ha alcun senso? Vale lo stesso per i nostri compagni caduti? Quei soldati non hanno avuto significato? No, non è così! A dare loro un significato siamo noi! Quei valorosi e sfortunati caduti gli unici a poterli ricordare siamo noi, i vivi! Noi moriremo qui! E affideremo il nostro significato a chi è rimasto in vita! Questo è l’unico modo che abbiamo per lottare contro questo mondo crudele!"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <p>
+            <?php
+                echo $var1;
+            ?>
+    </main>
+</body>
+</html>
